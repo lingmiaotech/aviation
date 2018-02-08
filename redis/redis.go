5e9901654fe4ff541367e0937ebb41c9ce03c30d
xuyuhang <yuhang.xu@0mzl.com>
2018-02-08 10:01:27 +0800

    Fix redis pub/sub connection

--- redis/redis.go
+++ redis/redis.go
@@ -34,11 +34,13 @@ func GetPub() *redis.Client {
 	host := configs.GetString("redis.host")
 	port := configs.GetInt("redis.port")
 	db := configs.GetInt("redis.db")
+	password := configs.GetDynamicString("redis.password")
 
 	PubClient := redis.NewClient(&redis.Options{
-		Addr:     fmt.Sprintf("%s:%d", host, port),
-		Password: "",
-		DB:       db,
+		Addr:       fmt.Sprintf("%s:%d", host, port),
+		Password:   password,
+		DB:         db,
+		MaxRetries: 3,
 	})
 
 	return PubClient
@@ -48,11 +50,13 @@ func GetSub() *redis.Client {
 	host := configs.GetString("redis.host")
 	port := configs.GetInt("redis.port")
 	db := configs.GetInt("redis.db")
+	password := configs.GetDynamicString("redis.password")
 
 	SubClient := redis.NewClient(&redis.Options{
-		Addr:     fmt.Sprintf("%s:%d", host, port),
-		Password: "",
-		DB:       db,
+		Addr:       fmt.Sprintf("%s:%d", host, port),
+		Password:   password,
+		DB:         db,
+		MaxRetries: 3,
 	})
 
 	return SubClient
