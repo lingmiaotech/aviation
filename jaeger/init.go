a05b1337063ae5ae1a5c78f2058e5da4dcc5cfaf
duan <xin.duan@echocloud.com>
2018-10-12 16:13:19 +0800

    feat: add jaeger init

--- jaeger/init.go
+++ jaeger/init.go
@@ -2,7 +2,9 @@ package jaeger
 
 import (
 	"fmt"
+	"os"
 	"strings"
+	"strconv"
 	"time"
 
 	"github.com/lingmiaotech/tonic/configs"
@@ -14,7 +16,18 @@ import (
 
 func InitJaeger() {
 
-	backendHostPort := fmt.Sprintf("%s:%s", configs.GetString("backendHostPort.host"), configs.GetString("backendHostPort.port"))
+	agentURI := os.Getenv("JAEGER_AGENT")
+	if agentURI == "" {
+		agentURI = fmt.Sprintf("http://jaeger-agent.%s:6831", configs.GetString("app_name"))
+	}
+
+	var backendHostPort string
+	env, _ := configs.Get("env").(string)
+	if env == "development" {
+		backendHostPort = "127.0.0.1:6831"
+	} else {
+		backendHostPort = agentURI
+	}
 
 	var err error
 	var sender jaeger.Transport
@@ -30,20 +43,22 @@ func InitJaeger() {
 		}
 	}
 
-	var sampler config.SamplerConfig
-	env := getServerMode()
-	switch env {
-	case "production":
-		sampler.Type = "const"
-		sampler.Param = 1
-	default:
-		sampler.Type = "const"
-		sampler.Param = 1
+	sampleType := os.Getenv("JAEGER_SAMPLE_TYPE")
+	if sampleType == "" {
+		sampleType = "probabilistic"
 	}
+	sampleParamString := os.Getenv("JAEGER_SAMPLE_PARAM")
+	if sampleParamString == "" {
+		sampleParamString = "0.5"
+	}
+	sampleParam, _ := strconv.ParseFloat(sampleParamString, 64)
 
 	cfg := &config.Configuration{
 		ServiceName: configs.GetString("app_name"),
-		Sampler: &sampler,
+		Sampler: &config.SamplerConfig{
+			Type:  sampleType,
+			Param: sampleParam,
+		},
 	}
 	tracer, _, err := cfg.NewTracer(
 		config.Reporter(jaeger.NewRemoteReporter(
@@ -53,14 +68,6 @@ func InitJaeger() {
 	if err != nil {
 		panic(fmt.Sprintf("ERROR: cannot init Jaeger: %v\n", err))
 	}
-	opentracing.SetGlobalTracer(tracer)
+	opentracing.InitGlobalTracer(tracer)
 	return
-}
-
-func getServerMode() string {
-	env, ok := configs.Get("env").(string)
-	if !ok {
-		logging.GetDefaultLogger().Warn("Cannot find env setting in config, will use development mode.")
-	}
-	return env
 }
